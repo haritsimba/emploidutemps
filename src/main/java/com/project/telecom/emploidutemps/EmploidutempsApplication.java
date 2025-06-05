9e8969a6d95927a606fb6f9628615aeb05f4a618
haritsimba <haritsimbafranck@gmail.com>
2025-06-05 13:37:27 +0300

    first commit

---
+++ src/main/java/com/project/telecom/emploidutemps/EmploidutempsApplication.java
@@ -0,0 +1,13 @@
+package com.project.telecom.emploidutemps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploidutempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploidutempsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/telecom/emploidutemps/EmploidutempsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.telecom.emploidutemps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploidutempsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
